980f783133cb2754d7658ecd98ff7fe00a56c53a
AlphaQuinn <snowboarderquinn@gmail.com>
2023-02-26 10:23:52 -0500

    adjusted

--- GenerateMarkdown.js
+++ GenerateMarkdown.js
@@ -1,7 +1,7 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  return '[license Badges[(https://img.shields.io/badge/LICENSE-${license}-blue'
+  return '[license Badges[(https://img.sheilds.io/badge/LICENCE-${license}-blue)'
    
   }
   
